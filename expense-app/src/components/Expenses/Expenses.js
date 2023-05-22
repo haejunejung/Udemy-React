15704dbbbd9d785fcbb6aa1162270941f1b14d33
haejunejung <jhj9422@unist.ac.kr>
2023-05-23 00:07:53 +0900

    expense-app

--- expense-app/src/components/Expenses/Expenses.js
+++ expense-app/src/components/Expenses/Expenses.js
@@ -1,10 +1,11 @@
 import React, { useState } from "react";
 import "./Expenses.css";
-import ExpenseItem from "./ExpenseItem";
+
 import ExpensesFilter from "./ExpensesFilter";
-import Card from "../UI/Card";
 import ExpensesList from "./ExpensesList";
 import ExpenseChart from "./ExpenseChart";
+
+import Card from "../UI/Card";
 
 const Expenses = (props) => {
   const [selectedYear, setSelectedYear] = useState("2023");
@@ -17,28 +18,11 @@ const Expenses = (props) => {
     return expense.date.getFullYear().toString() === selectedYear;
   });
 
-  const expenses =
-    filteredExpenses.length === 0 ? (
-      <p>No expenses found.</p>
-    ) : (
-      filteredExpenses.map((expense) => {
-        return (
-          <ExpenseItem
-            key={expense.id}
-            date={expense.date}
-            title={expense.title}
-            amount={expense.amount}
-          />
-        );
-      })
-    );
-
   return (
     <Card className="expenses">
       <ExpensesFilter selected={selectedYear} onSaveYear={saveYearHandler} />
       <ExpenseChart expenses={filteredExpenses} />
       <ExpensesList filteredExpenses={filteredExpenses} />
-      {expenses}
     </Card>
   );
 };
